db2bf09de701097c52f9e8e376e80a9f49dc4652
Merdiplay <39810142+Merdiplay@users.noreply.github.com>
2018-06-02 14:19:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,9 +114,8 @@ client.on("message", async message => {
     const deleteCount = parseInt(args[0], 10);
  
     
- 
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    if(!deleteCount || deleteCount < 1 || deleteCount > 100)
+      return message.reply("Please provide a number between 1 and 100 for the number of messages to delete");
     
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
