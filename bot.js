75571a1d22fa203bb7bb129b66b4fb9877660c58
Merdiplay <39810142+Merdiplay@users.noreply.github.com>
2018-07-30 23:55:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`with his Sharingan`);
+  client.user.setActivity(`E`);
 });
 
 client.on("guildCreate", guild => {
