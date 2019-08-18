3d680d1f375b177ba513d82f3591c7e9d9251d95
Merdiplay <39810142+Merdiplay@users.noreply.github.com>
2019-08-18 16:12:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -212,16 +212,15 @@ client.on("message", async message => {
 
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
 
-    
-
-    // So we get our messages, and delete them. Simple enough, right?
-
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
 
     message.channel.bulkDelete(fetched)
 
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-
+  }
+    else{
+  message.channel.send(`Wot? I couldnt understand you.`);
+  return;
   }
 
 });
