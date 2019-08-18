a90709b37c3c0faa3f00e6118babb595dbf43182
Merdiplay <39810142+Merdiplay@users.noreply.github.com>
2019-08-18 15:11:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,17 +8,17 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Yeet`,client.user.);
+  client.user.setActivity(`Serving ${client.users.size} Member| Made by Merdi`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Yeet`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Yeet`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 
