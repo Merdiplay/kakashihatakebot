049221f7e323822a44cb08f9fda52564ddbc420c
Merdiplay <39810142+Merdiplay@users.noreply.github.com>
2019-08-18 15:16:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,61 +31,21 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  
-  if(command === "say") {
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 
-    const sayMessage = args.join(" ");
-
-    message.delete().catch(O_o=>{}); 
-
-    message.channel.send(sayMessage);
   }
   
 
-	if (message.content === '!ping') {
-
-		message.channel.send('Pong.');
-
-	}
-
+	
 });
-  
-  if(command === "kick") {
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-  
-  
-  
-if(command === "forbidden-jutsu") {
-message.channel.send(`https://media1.tenor.com/images/07eb87ae016a48197c99bcf2bc3879a6/tenor.gif?itemid=7360766`);
-return;
-}
-
- 
-  
-     if(command === "ashamed") {
- message.channel.send("https://media.giphy.com/media/JogBsleaWauHe/giphy.gif");                                     
+   
+       if(command === "forbidden-jutsu") {
+ message.channel.send(`https://media1.tenor.com/images/07eb87ae016a48197c99bcf2bc3879a6/tenor.gif?itemid=7360766`);
  return;
  }
-  
+ 
        if(command === "kamui") {
  message.channel.send("https://media.giphy.com/media/aMhiY5TgVAmVG/giphy.gif");                                     
  return;
@@ -93,9 +53,9 @@ return;
 
   
     else{
-    message.channel.send(`Wot? I couldnt understand you.`);
-    return;
-         }
+  message.channel.send(`Wot? I couldnt understand you.`);
+  return;
+  }
       
   
 });
